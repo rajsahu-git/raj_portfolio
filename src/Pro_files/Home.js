7be8f000a4ba00e19b2da02e9a19ca5624cd316e
raju8824 <{55652441}+{rajsahu-git}@users.noreply.github.com>
2024-07-16 18:22:40 +0530

    Add projects

--- src/Pro_files/Home.js
+++ src/Pro_files/Home.js
@@ -1,12 +1,16 @@
 import NavBar from "./NavBar"
 import Header from "./Header"
 import Skill from "./Skill"
+import My_work from "./My_work"
+
+
 function Home(){
     return(
         <div>
             <NavBar />
             <Header />
             <Skill/>
+            <My_work/>
             {/*About me
             Projects
             Contact me
